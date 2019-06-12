8c557d444255cf697a17bebdd843357687c16a4b
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-12 14:28:26 +0100

    Add Friend component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './friends.css';
+
+export default function Friend(props) {
+    const { name, age, email } = props;
+
+    return (
+        <div>
+            <span>{name}</span>
+            <span>{age}</span>
+            <span>{email}</span>
+        </div>
+    )
+}
