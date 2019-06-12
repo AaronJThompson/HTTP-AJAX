50bdb5e3e9420304923c612e57280a5c6642e951
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-12 14:30:12 +0100

    Add FriendList componennt

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './friends.css';
+import Friend from './Friend';
+
+export default function FriendList(props) {
+    let { friends } = props;
+
+    return (
+        <div>
+            {
+                friends.map((friend) => <Friend {...friend} />)
+            }
+        </div>
+    )
+
+}
