e53fabf81f9647a44e738f536eddb12e5a8f0b5b
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-12 15:03:34 +0100

    Start input form

--- friends/src/components/FriendFrom.js
+++ friends/src/components/FriendFrom.js
@@ -1,2 +1,35 @@
 import React from 'react';
 import './friends.css';
+import styled from 'styled-components';
+
+export default function FriendForm(props) {
+    let { addFriend, nameInput, nameValue, ageInput, ageValue, emailInput, emailValue } = props;
+
+    return (
+        <form>
+            <input
+                type='text'
+                placeholder='name'
+                value={nameValue}
+                onChange={nameInput}
+            />
+            <input
+                type='text'
+                placeholder='age'
+                value={ageValue}
+                onChange={ageInput}
+            />
+            <input
+                type='text'
+                placeholder='email'
+                value={emailValue}
+                onChange={emailInput}
+            />
+            <input
+                type='submit'
+                value='Add Friend'
+                onClick={addFriend}
+            />
+        </form>
+    )
+}
