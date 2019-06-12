dbdadd582a7e025a358de0ac667abcc5062b58b5
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-12 14:52:57 +0100

    Add basic styling to friends

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import './friends.css';
+
+const StyledFriend = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: space-around;
+    margin: 0 auto;
+    margin-bottom: 1rem;
+    background-color: blue;
+    color: white;
+    border-radius: 5px;
+    height: 2rem;
+    align-items: center;
+    
+    span {
+        text-align: center;
+        width: 30%;
+    }
+`;
 
 export default function Friend(props) {
     const { name, age, email } = props;
 
     return (
-        <div>
+        <StyledFriend>
             <span>{name}</span>
             <span>{age}</span>
             <span>{email}</span>
-        </div>
+        </StyledFriend>
     )
 }
